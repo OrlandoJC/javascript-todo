28b7199510c13f076568384f0de96815d6f1770a
Orlando de jesus <cuximyamaorlandodejesus@gmail.com>
2018-01-19 17:14:40 -0600

    Update todo with localStorage
    Data persistence with local storage

--- js/todo.js
+++ js/todo.js
@@ -1,79 +1,114 @@
 /******************************************
  *  Author : orlando   
  *  Created On : Tue Jan 09 2018
- *  File : todo.js
+ *  File : productos.js
  *******************************************/
-var ul = document.getElementById('list');
-var input = document.getElementById('search-box');
-var deleteAll = document.getElementById('add-task');
-var alert = document.getElementById('warning-alert');
-var isAllDeleted = false;
 
-function makeTask(task) {
+;
+(function() {
+    var ul = document.getElementById('list');
+    var input = document.getElementById('search-box');
+    var deleteAll = document.getElementById('add-task');
+    var alert = document.getElementById('warning-alert');
+    var isAllDeleted = false;
 
-    var text = document.createTextNode(task),
-        deleteText = document.createTextNode('X'),
-        item = document.createElement('li'),
-        deletButton = document.createElement('span');
+    if (localStorage.getItem('tasks') !== null) {
+        var localStorageTasks = JSON.parse(localStorage.getItem("tasks"));
 
-    deletButton.appendChild(deleteText);
-    item.appendChild(deletButton);
-    item.appendChild(text);
-    ul.appendChild(item);
+        localStorageTasks.forEach(function(el) {
+            makeTask(el);
+        })
+    }
 
-}
+    function setLocalStorage(task) {
+        var tasks = getLocalStorage();
+        tasks.push(task);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
 
-function deleteAllTask() {
 
-    var items = Array.prototype.slice.call(document.querySelectorAll('li'));
+    function deleteLocaltorage(element) {
+        var allTasksLocalStorage = getLocalStorage();
 
-    items.forEach(function(task) {
-        if (task.classList[0] === "completed") {
-            ul.removeChild(task);
+        allTasksLocalStorage.forEach(function(el, index) {
+            if (el === element) {
+                allTasksLocalStorage.splice(index, 1);
+            }
+        })
+
+        localStorage.setItem('tasks', JSON.stringify(allTasksLocalStorage));
+    }
+
+    function getLocalStorage() {
+        var tasks;
+        if (localStorage.getItem('tasks') === null) {
+            tasks = [];
+        } else {
+            tasks = JSON.parse(localStorage.getItem("tasks"));
         }
+        return tasks;
+    }
+
+    function makeTask(task) {
+
+        var text = document.createTextNode(task),
+            deleteText = document.createTextNode('X'),
+            item = document.createElement('li'),
+            deletButton = document.createElement('span');
+
+        deletButton.appendChild(deleteText);
+        item.appendChild(deletButton);
+        item.appendChild(text);
+        ul.appendChild(item);
+    }
+
+    function deleteAllTask() {
+        var items = Array.prototype.slice.call(document.querySelectorAll('li'));
+
+        items.forEach(function(task) {
+            if (task.classList[0] === "completed") {
+                ul.removeChild(task);
+            }
+        })
+        isAllDeleted = true;
+    }
+
+    function alertDeleted() {
+        alert.classList.remove('hidde');
+
+        var interval = setInterval(function() {
+            alert.classList.add('hidde');
+            clearInterval(interval);
+        }, 2000);
+    }
+
+    deleteAll.addEventListener('click', function() {
+        if (isAllDeleted) alertDeleted();
+        else deleteAllTask();
     })
 
-    isAllDeleted = true;
+    input.addEventListener('change', function() {
+        makeTask(input.value);
+        setLocalStorage(input.value);
+        input.value = "";
+    })
 
-}
+    ul.addEventListener('click', function(el) {
+        //elemento en la lista
+        if (el.target.nodeName === 'LI') {
+            el.target.classList.toggle('completed');
+            isAllDeleted = false;
+        }
+        //boton de borrado de cada elemento
+        if (el.target.nodeName === 'SPAN') {
+            var element = el.target.parentNode.textContent.substring(1, el.target.parentNode.textContent.length);
+            var localStorageTasks = getLocalStorage();
 
-function alertDeleted() {
-
-    alert.classList.remove('hidde');
-
-    var interval = setInterval(function() {
-        alert.classList.add('hidde');
-        clearInterval(interval);
-    }, 2000);
-
-}
-
-deleteAll.addEventListener('click', function() {
-
-    if (isAllDeleted) {
-        alertDeleted();
-    } else {
-        deleteAllTask();
-    }
-
-})
-
-input.addEventListener('change', function() {
-
-    makeTask(input.value);
-    input.value = "";
-
-})
-
-ul.addEventListener('click', function(el) {
-
-    if (el.target.nodeName === 'LI') {
-        el.target.classList.toggle('completed');
-        isAllDeleted = false;
-    }
-
-    if (el.target.nodeName === 'SPAN') {
-        ul.removeChild(el.target.parentNode);
-    }
-
-})
+            if (localStorageTasks.includes(element)) {
+                deleteLocaltorage(element)
+            } else {
+                ul.removeChild(el.target.parentNode);
+            }
+        }
+    })
+})();
